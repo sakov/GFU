af55287869d68ee369d9252348419fee32680c41
sakov <pavel.sakov@gmail.com>
2025-04-09 13:27:36 +1000

    cosmetics in regrid_ll.c

--- apps/regrid_ll.c
+++ apps/regrid_ll.c
@@ -67,7 +67,7 @@ static void usage(int status)
     printf("    -v <varname> -- variable to interpolate\n");
     printf("    -gi <src grid> <lon> <lat> [<numlayers>] -- source grid\n");
     printf("    -go <dst grid> <lon> <lat> [<numlayers>] -- destination grid\n");
-    printf("    -d <level> -- deflation level (as in src)\n");
+    printf("    -d <level> -- deflation level (default = as in src)\n");
     printf("    -m -- flag: use NaN for filling (default = use zero)\n");
     printf("    -n -- flag: use the deepest valid value for filling the rest of the column\n");
     printf("    -s -- flag: do not use the first and last columns of the source field\n");
